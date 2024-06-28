effabeb270b7a5a2cd5d978ac56c4440f4030e9b
Shogo Ogami <sg.tao.so@gmail.com>
2024-06-28 21:19:24 +0900

    Add:update suppl_datavisual_app page for append some graphs

--- src/pages/12_suppl_datavisual_app.py
+++ src/pages/12_suppl_datavisual_app.py
@@ -3,6 +3,7 @@ import sys
 import os
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # srcディレクトリをモジュール検索パスに追加
 sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
@@ -27,4 +28,24 @@ st.caption("これはサプーの動画用テストアプリです")
 
 # データフレーム読み出し
 df = pd.read_csv("assets/data/temperature.csv", index_col="月")
+
+st.subheader("st.dataframe(df)")
 st.dataframe(df)
+st.subheader("st.table(df)")
+st.table(df)
+
+# 折れ線グラフ
+st.subheader("st.line_chart(df)")
+st.line_chart(df)
+
+# 棒グラフ
+st.subheader("st.bar_chart(df['2022年'])")
+st.bar_chart(df["2022年"])
+
+
+# matplotlib
+st.subheader("using matplotlib")
+fig, ax = plt.subplots()
+ax.plot(df.index, df["2022年"])
+ax.set_title("matplotlib graph")
+st.pyplot(fig)
